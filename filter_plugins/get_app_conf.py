ca8af3393e888fd4129f55d53bfc480c7ae531b5
Kevin Veen-Birkenbach <kevin@veen.world>
2025-12-17 13:20:11 +0100

    Solved errors in unit tests appearing through ruff autofix

--- filter_plugins/get_app_conf.py
+++ filter_plugins/get_app_conf.py
@@ -1,11 +1,34 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from module_utils.config_utils import get_app_conf
+from module_utils.config_utils import (
+    AppConfigKeyError,
+    ConfigEntryNotSetError,
+    get_app_conf as _get_app_conf,
+)
 
-class FilterModule(object):
-    ''' Infinito.Nexus application config extraction filters '''
+__all__ = [
+    "get_app_conf",
+    "AppConfigKeyError",
+    "ConfigEntryNotSetError",
+]
+
+
+def get_app_conf(
+    applications,
+    application_id,
+    config_path,
+    strict=True,
+    default=None,
+    skip_missing_app=False,
+):
+    return _get_app_conf(
+        applications=applications,
+        application_id=application_id,
+        config_path=config_path,
+        strict=strict,
+        default=default,
+        skip_missing_app=skip_missing_app,
+    )
+
+
+class FilterModule:
     def filters(self):
-        return {
-            'get_app_conf': get_app_conf,
-        }
+        return {"get_app_conf": get_app_conf}
--- tests/unit/filter_plugins/test_get_app_conf.py
+++ tests/unit/filter_plugins/test_get_app_conf.py
@@ -4,7 +4,8 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../filter_plugins')))
 
-from get_app_conf import get_app_conf, AppConfigKeyError
+from get_app_conf import get_app_conf
+from module_utils.config_utils import AppConfigKeyError
 from ansible.errors import AnsibleFilterError
 
 class TestGetAppConf(unittest.TestCase):
