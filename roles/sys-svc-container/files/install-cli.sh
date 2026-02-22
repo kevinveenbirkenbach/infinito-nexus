13c41ec86d317fb7eeb70a219ade39b81f74c20d
Kevin Veen-Birkenbach <kevin@veen.world>
2026-02-23 00:59:26 +0100

    fix(sys-svc-container): switch Fedora docker CLI install to Docker CE

--- roles/sys-svc-container/files/install-cli.sh
+++ roles/sys-svc-container/files/install-cli.sh
@@ -45,7 +45,10 @@ elif [[ "${ID}" == "debian" || "${ID}" == "ubuntu" || "${ID_LIKE:-}" =~ debian ]
   rm -rf /var/lib/apt/lists/*
 
 elif [[ "${ID}" == "fedora" ]]; then
-  (dnf -y install docker-cli) || (dnf -y install moby-engine)
+  dnf -y install dnf-plugins-core
+  dnf config-manager --add-repo "https://download.docker.com/linux/fedora/docker-ce.repo"
+  dnf -y remove docker-cli docker-buildx moby-engine moby-filesystem || true
+  dnf -y install --allowerasing docker-ce-cli docker-buildx-plugin docker-compose-plugin
   dnf -y clean all
   rm -rf /var/cache/dnf
 
